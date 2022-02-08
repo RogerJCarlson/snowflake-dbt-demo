64fc0f5947c2847092db5be218e4c13650c0da96
Carlson, Roger J <Roger.Carlson@spectrumhealth.org>
2022-02-08 09:35:15 -0500

    updated import_participants

--- macros/import_participants.sql
+++ macros/import_participants.sql
@@ -1,22 +1,15 @@
 {%- macro import_participants() -%}
 
   {%- set query -%}
-    REMOVE @CDM.AOU_PARTICIPANTS
-  {%- endset -%}
-  {%- do run_query(query) -%}
 
-  {%- set query -%}
-    PUT 'FILE://X:/ALL OF US/OMOP_WORKING/EXPORT/ALLOFUSPARTICIPANT.CSV' @CDM.AOU_PARTICIPANTS
-  {%- endset -%}
-  {%- do run_query(query) -%}
+    REMOVE @CDM.AOU_PARTICIPANTS;
 
-  {%- set query -%}
-    TRUNCATE TABLE OMOP.ALLOFUSPARTICIPANTS
-  {%- endset -%}
-  {%- do run_query(query) -%}
+    PUT 'FILE://X:/ALL OF US/OMOP_WORKING/EXPORT/ALLOFUSPARTICIPANT.CSV' @CDM.AOU_PARTICIPANTS;
+ 
+    TRUNCATE TABLE OMOP.ALLOFUSPARTICIPANTS;
 
-  {%- set query -%}
-    COPY INTO OMOP.ALLOFUSPARTICIPANTS FROM '@CDM.AOU_PARTICIPANTS/AllOfUSParticipant.csv.gz' FILE_FORMAT = 'CDM.OMOP_CSV_FILE_FORMAT' ON_ERROR = 'ABORT_STATEMENT' PURGE = FALSE;
+    COPY INTO OMOP.ALLOFUSPARTICIPANTS FROM '@CDM.AOU_PARTICIPANTS/ALLOFUSPARTICIPANT.CSV.gz' FILE_FORMAT = 'CDM.OMOP_CSV_FILE_FORMAT' ON_ERROR = 'ABORT_STATEMENT' PURGE = FALSE;
+
   {%- endset -%}
   {%- do run_query(query) -%}
 
